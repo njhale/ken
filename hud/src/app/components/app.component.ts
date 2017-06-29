ab85c27f7af5eb3455515d0ee9324c9a70312de1
NicholasCostanzo <costanzo.nicholas.j@gmail.com>
2017-06-29 14:43:15 -0400

    feat: implement basic aframe

--- hud/src/app/components/app.component.ts
+++ hud/src/app/components/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  elem: any;
+  aframe: any;
+  timeout: any;
+
+
+  constructor(ref: ElementRef) {
+      this.elem = ref.nativeElement;
+  }
+
+  ngOnInit() {
+      this.aframe = this.elem.querySelector('a-scene');
+  }
 }
