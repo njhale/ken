9dccb7567b20d1c90843919cf2ca08081fe86bc9
NJHale <njh4@njit.edu>
2017-06-29 15:25:21 -0400

    Add server.js

---
+++ hud/server.js
@@ -0,0 +1,45 @@
+/*******************
+* REQUIRED MODULES *
+*******************/
+var express = require('express');
+var http = require('http');
+var app = express();
+var proxy = require('express-http-proxy');
+var url = require('url');
+
+// Get the config module
+var config = require('./config');
+
+var server = http.createServer(app);
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+/**************************************************
+* REGISTER PORT 8080 FOR APPLICATION TO LISTEN ON *
+**************************************************/
+server.listen(8080, function () {
+  console.log('App listening on port 8080!');
+});
+
+/*******************************************************************
+* ACQUIRE ENVIRONMENT VARIABLES FOR HOST:PORT TO PROXY REQUESTS TO *
+*******************************************************************/
+
+
+/****************************************************************
+* REGISTER DIRECTORY CONTENT TO BE VIEWED BY APP AS / DIRECTORY *
+****************************************************************/
+
+//dist folder that typescript compiles to
+app.use(express.static(__dirname + '/dist'));
+
+/**********
+* ROUTING *
+**********/
+
+/**
+* Redirect pages used by the frontend to index.html for everything else
+*/
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + "/dist/index.html");
+});
