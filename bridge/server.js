e7bcc969cfb7b5d67ab675d00eb306aceb4a9fef
Mark Falto <mfalto@gmail.com>
2017-06-29 17:47:30 -0400

    updated

--- bridge/server.js
+++ bridge/server.js
@@ -9,6 +9,36 @@ const PORT = process.env.BRIDGE_SERVICE_PORT || 8080;
 const HOST = process.env.BRIDGE_SERVICE_HOST || 'localhost';
 
 
+
+
+io.on('connection', function (socket) {
+  console.log(`connection confirmed from ${socket.id}`);
+
+})
+
+app.get('/register', (req, res) => {
+  try {
+    console.log('register url hit ' + APP_POD_NAME);
+    // Return a 200 'OK'
+    io.emit('register', { "name": 'nichhale@gmail.com', "position":[ 0, 0, 0], "MyDate":"@1269388885866@" });
+    res.status(200).send(`registration complete ${APP_POD_NAME}`);
+  } catch (err) {
+    console.log(`Something went wrong while registering a client /: ${err}`);
+  }
+});
+
+
+app.get('/whereabouts', (req, res) => {
+  try {
+    console.log('whereabouts url hit ' + APP_POD_NAME);
+    // Return a 200 'OK'
+    io.emit('whereabouts', { "name": 'nichhale@gmail.com', "position":[ 0, 0, 0], "MyDate":"@1269388885866@" });
+    res.status(200).send(`registration complete ${APP_POD_NAME}`);
+  } catch (err) {
+    console.log(`Something went wrong while registering a client /: ${err}`);
+  }
+});
+
 /**
 * Readiness health-check endpoint
 **/
