28b8ff7b30fad976d66a0ceee96424981a315aa9
NJHale <njh4@njit.edu>
2017-06-30 00:48:56 -0400

    Moomin

--- hud/src/app/app.component.ts
+++ hud/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
-import cameraSource from './camera.service';
+import { CameraSource } from './camera.service';
+
 
 @Component({
   selector: 'app-root',
@@ -11,17 +12,19 @@ export class AppComponent implements OnInit {
   elem: any;
   aframe: any;
   timeout: any;
+  cameraSource: CameraSource;
 
 
   constructor(ref: ElementRef) {
     console.log('here we are!');
     this.elem = ref.nativeElement;
+    this.cameraSource = new CameraSource();
   }
 
   startCamera(id: any) {
     var vid = document.querySelector('#inputVideo');
 
-    cameraSource.start({
+    this.cameraSource.start({
       videoElement: vid,
       constraints: {
         video: { deviceId: id ? { exact: id } : undefined }
@@ -36,10 +39,12 @@ export class AppComponent implements OnInit {
     console.log('This is ng init!');
 
     this.aframe = this.elem.querySelector('a-scene');
-    navigator.mediaDevices.enumerateDevices().then(function(devices) {
+    console.log('Got the f-ing aframe')
+    navigator.mediaDevices.enumerateDevices().then((devices) => {
       var id;
+      console.log('In promise');
       devices.forEach(function(device) {
-        //console.log(device.kind": "  device.label " id = "  device.deviceId);
+        console.log(device.kind, ": ",  device.label, " id = ",  device.deviceId);
         if (device.kind === 'videoinput') {
           id = device.deviceId;
         }
