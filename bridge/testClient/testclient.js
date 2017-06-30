e1027205c65985569ef316f01129cd902e9df1d4
Mark Falto <markfalto@markfalto.com>
2017-06-30 00:59:19 -0400

    changed timeout to simpler implmeentation

--- bridge/testClient/testclient.js
+++ bridge/testClient/testclient.js
@@ -3,13 +3,14 @@ function Client(io,util) {
 
   this.start = () => {
     var that = this;
-    setTimeoutPromise(1000, 'foobar').then((value) => {
+    setTimeout(() => {
       // value === 'foobar' (passing values is optional)
       // This is executed after about 40 milliseconds.
       console.log('time tripped');
       io.emit('clientwhereabouts', { "name": 'timedClient@gmail.com', "position":[ 41.0662516,-74.1727549, 0], "MyDate":"@1269388885866@" });
       that.start();
-    });
+    }, 1000);
+
   };
 
 
