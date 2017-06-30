ba6875a28dccf8dd9c170f801f0ad987df03ed3c
NJHale <njh4@njit.edu>
2017-06-30 01:57:36 -0400

    kranged it up

--- bridge/testClient/testclient.js
+++ bridge/testClient/testclient.js
@@ -6,7 +6,7 @@ function Client(io,util) {
       // value === 'foobar' (passing values is optional)
       // This is executed after about 40 milliseconds.
       console.log('time tripped');
-      io.emit('clientwhereabouts', { "name": 'timedClient@gmail.com', "position":[ 41.0662516,-74.1727549, 0], "tm": new Date() });
+      io.emit('whereabouts', { "name": 'krang', "position":[ 41.0662516,-74.1727549, 0], "tm": new Date() });
       that.start();
     }, 1000);
 
